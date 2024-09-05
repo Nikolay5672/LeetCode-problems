ee5a72031f1da201b99baa3e0431c66c255ab7eb
Haitax123 <nikolay_5672@mail.bg>
2024-09-05 18:43:27 +0300

    added merge sorted arrays

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String s = "1337c0d3";
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3;
+        int n = 3;
+        MergeSortedArrays.merge(nums1, m, nums2, n);
 
-        System.out.println(StringToIntegerAtoi.myAtoi(s));
+        System.out.println(Arrays.toString(nums1));
 
     }
 }
---
+++ src/MergeSortedArrays.java
@@ -0,0 +1,31 @@
+public class MergeSortedArrays {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Start filling nums1 from the last index.
+        int lastIndex = m + n - 1;
+
+        // Pointers for nums1 and nums2.
+        int i = m - 1;  // Pointer for the last element in nums1's valid part.
+        int j = n - 1;  // Pointer for the last element in nums2.
+
+        // Merge nums2 into nums1, starting from the back.
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[lastIndex] = nums1[i];
+                i--;
+            } else {
+                nums1[lastIndex] = nums2[j];
+                j--;
+            }
+            lastIndex--;
+        }
+
+        // If nums2 still has elements left, copy them.
+        while (j >= 0) {
+            nums1[lastIndex] = nums2[j];
+            j--;
+            lastIndex--;
+        }
+
+        // If nums1 has elements left, they're already in place.
+    }
+}
