b466bf1e28acd0884c72f4eda9f6d1ce35961084
Haitax123 <nikolay_5672@mail.bg>
2024-09-07 17:52:54 +0300

    added remove duplicates from sorted array

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3, 2, 2, 3};
-        int m = 3;
+        int[] nums = {1, 2, 2, 3, 3, 3};
 
-
-        System.out.println(RemoveElement.removeElement(nums, m));
+        System.out.println(RemoveDuplicatesFromSortedArray.removeDuplicates(nums));
         System.out.println(Arrays.toString(nums));
 
     }
---
+++ src/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,27 @@
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        // If the array is empty, return 0
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        // Pointer `k` to keep track of the position of the unique elements
+        int k = 1;  // Start from the second element since the first one is always unique
+
+        // Iterate through the array from the second element onwards
+        for (int i = 1; i < nums.length; i++) {
+            // If the current element is different from the previous one
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];  // Move the unique element to position `k`
+                k++;  // Increment the count of unique elements
+            }
+        }
+
+        return k;  // `k` is the number of unique elements
+
+    }
+}
+
+
+
+// 1 1 2 3 4 4 4 - 1 2 3 4
