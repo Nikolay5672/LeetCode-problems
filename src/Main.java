27e64664e7601a33e7a10ce30d54e45303f07f29
Haitax123 <nikolay_5672@mail.bg>
2024-09-07 17:26:12 +0300

    added remove item solution

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
+        int[] nums = {3, 2, 2, 3};
         int m = 3;
-        int n = 3;
-        MergeSortedArrays.merge(nums1, m, nums2, n);
 
-        System.out.println(Arrays.toString(nums1));
+
+        System.out.println(RemoveElement.removeElement(nums, m));
+        System.out.println(Arrays.toString(nums));
 
     }
 }
---
+++ src/RemoveElement.java
@@ -0,0 +1,15 @@
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0; // pointer to track the position of non-val elements
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // move the non-val element to the k-th position
+                k++; // increment the count of valid elements
+            }
+        }
+
+        return k; // return the number of elements not equal to val
+
+    }
+}
