2bb07d087656ba973a720b5c01b42ce0398d5b0c
Haitax123 <nikolay_5672@mail.bg>
2024-09-08 18:13:45 +0300

    added best time to buy and sell stock solution

---
+++ src/BestTImeToBuyAndSellStock.java
@@ -0,0 +1,19 @@
+public class BestTImeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            // Update the minimum price encountered so far
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                // Calculate the profit and update the maxProfit if necessary
+                int profit = price - minPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+        }
+
+        return maxProfit;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3, 2, 3,};
+        int[] nums = {7, 1, 5, 3, 6, 4};
 
-        System.out.println(MajorityElement.majorityElement(nums));
+        System.out.println(BestTImeToBuyAndSellStock.maxProfit(nums));
         System.out.println(Arrays.toString(nums));
 
     }
