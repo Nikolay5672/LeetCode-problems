8826b7baa9a16d8f165b47dfc598d7237afe30c7
Haitax123 <nikolay_5672@mail.bg>
2024-09-09 18:01:41 +0300

    added jump game solution

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] nums = {1, 2, 4, 0, 0, 0, 7};
 
-        RotateArray.rotateUpdated(nums, 3);
+        System.out.println(JumpGame.canJump(nums));
         System.out.println(Arrays.toString(nums));
 
     }
