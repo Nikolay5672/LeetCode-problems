ba57e749b215dbfe44ed6e3c35afac6ad37b34c6
Haitax123 <nikolay_5672@mail.bg>
2024-09-11 17:22:06 +0300

    added length of last word solution

---
+++ src/Easy/LengthOfLastWord.java
@@ -0,0 +1,29 @@
+package Easy;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int counter = 0;
+        s = s.trim();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' '){
+                counter = 0;
+            }
+            else{
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static int lengthOfLastWordUpdated(String s) {
+        // Trim the string to remove leading and trailing spaces
+        s = s.trim();
+
+        // Find the index of the last space
+        int lastSpaceIndex = s.lastIndexOf(' ');
+
+        // The length of the last word is the difference between the string length and the last space index
+        return s.length() - lastSpaceIndex - 1;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import Medium.JumpGame;
-
+import Easy.LengthOfLastWord;
 import java.util.Arrays;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 4, 0, 0, 0, 7};
+        String s = "as sum bokluk";
 
-        System.out.println(JumpGame.canJump(nums));
+        System.out.println(LengthOfLastWord.lengthOfLastWord(s));
         System.out.println(Arrays.toString(nums));
 
     }
