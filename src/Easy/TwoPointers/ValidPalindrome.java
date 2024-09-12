60d48c809c40b18c16a628b3bdda633583905faf
Haitax123 <nikolay_5672@mail.bg>
2024-09-12 16:53:14 +0300

    added valid palindrome solution

---
+++ src/Easy/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,26 @@
+package Easy.TwoPointers;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        StringBuilder filtered = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filtered.append(Character.toLowerCase(c));
+            }
+        }
+        
+        // Check if the filtered string is a palindrome
+        int left = 0;
+        int right = filtered.length() - 1;
+        while (left < right) {
+            if (filtered.charAt(left) != filtered.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
